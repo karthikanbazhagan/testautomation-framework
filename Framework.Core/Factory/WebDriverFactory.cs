95c6e0ccc0872ac912105dc556f2fe55aeff3cdf
Karthik Anbazhagan <Karthik.Anbazhagan@cognizant.com>
2020-01-04 14:36:59 +0530

    Added Webdriver factory and utilities. Renamed CsvHelper with CsvDataHandler.

---
+++ Framework.Core/Factory/WebDriverFactory.cs
@@ -0,0 +1,53 @@
+﻿namespace Framework.Core.Factory
+{
+    using System;
+    using System.Configuration;
+    using System.Threading;
+    
+    using OpenQA.Selenium;
+
+    using Framework.Core.Utilities;
+
+    /// <summary>
+    /// Web driver factory to create and dispose an instance of the IWebdriver interface
+    /// </summary>
+    public class WebDriverFactory
+    {
+        private IWebDriver WebDriver;
+        
+        public IWebDriver GetWebDriver
+        {
+            get
+            {
+                return WebDriver ?? (WebDriver = CreateWebDriver());
+            }
+        }
+
+        public void DisposeWebDriver()
+        {
+            WebDriver.Quit();
+            WebDriver = null;
+        }
+
+        private IWebDriver CreateWebDriver()
+        {
+            using (var mutex = new Mutex(false, "CreateWebDriver"))
+            {
+                var mutexWaitTime = Convert.ToDouble(ConfigurationManager.AppSettings["MutexWaitTime"]);
+
+                mutex.WaitOne(TimeSpan.FromSeconds(mutexWaitTime));
+
+                try
+                {
+                    WebDriver = WebDriverUtilities.StartWebDriver();
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+
+            return WebDriver;
+        }
+    }
+}
