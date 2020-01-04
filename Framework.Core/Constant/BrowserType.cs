ac40e88b53946fe45affc64e714412387b56b80f
Karthik Anbazhagan <Karthik.Anbazhagan@cognizant.com>
2020-01-04 14:36:59 +0530

    Added Webdriver factory and utilities. Renamed CsvHelper with CsvDataHandler.

---
+++ Framework.Core/Constant/BrowserType.cs
@@ -0,0 +1,23 @@
+﻿namespace Framework.Core.Constant
+{
+    /// <summary>
+    /// Type of browser to test with
+    /// </summary>
+    internal enum BrowserType
+    {
+        /// <summary>
+        /// Internet Explorer browser
+        /// </summary>
+        InternetExplorer,
+
+        /// <summary>
+        /// Firefox browser
+        /// </summary>
+        Firefox,
+
+        /// <summary>
+        /// Chrome browser
+        /// </summary>
+        Chrome
+    }
+}
