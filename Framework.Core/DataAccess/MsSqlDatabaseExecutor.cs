90fd2b34f63820557e410b2c2a0d7c4b1cb8a0c7
Karthik Anbazhagan <Karthik.Anbazhagan@cognizant.com>
2020-02-17 16:58:10 +0530

    Added the below framework base components:     1. BaseDatbaseExecutor     2. BasePage And removed some of the duplicate class file which were removed from the project already but missed to delete the physical files.     1. SeleniumUtilities - Replaced with Selenium Extension     2. CsvHelper - Replaced with CsvDataHandler     3. SqlUtility - Optimised with DatabaseExecutors

---
+++ Framework.Core/DataAccess/MsSqlDatabaseExecutor.cs
@@ -0,0 +1,38 @@
+﻿namespace Framework.Core.DataAccess
+{
+    using System.Data;
+    using System.Data.SqlClient;
+
+    using Framework.Core.Constant;
+
+    public sealed class MsSqlDatabaseExecutor : BaseDatabaseExecutor
+    {
+        public MsSqlDatabaseExecutor()
+        {
+            this.DatabaseType = DatabaseType.MsSql;
+            this.ConnectionString = "Your connection string to the SQL Databse";
+        }
+
+        public override DataTable ExecuteQuery(string query)
+        {
+            DataTable table;
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        // Command timeout - 3 minutes
+                        adapter.SelectCommand.CommandTimeout = 180;
+                        table = new DataTable();
+                        adapter.Fill(table);
+                    }
+                }
+            }
+
+            return table;
+        }
+    }
+}
---
+++ Framework.Core/DataAccess/OracleDatabaseExecutor.cs
@@ -0,0 +1,39 @@
+﻿namespace Framework.Core.DataAccess
+{
+    using System.Data;
+
+    using Oracle.ManagedDataAccess.Client;
+
+    using Framework.Core.Constant;
+
+    public sealed class OracleDatabaseExecutor : BaseDatabaseExecutor
+    {
+        public OracleDatabaseExecutor()
+        {
+            this.DatabaseType = DatabaseType.Oracle;
+            this.ConnectionString = "Your connection string to the Oracle Databse";
+        }
+
+        public override DataTable ExecuteQuery(string query)
+        {
+            DataTable table;
+
+            using (var connection = new OracleConnection(ConnectionString))
+            {
+                using (var command = new OracleCommand(query, connection))
+                {
+                    connection.Open();
+                    using (var adapter = new OracleDataAdapter(command))
+                    {
+                        // Command timeout - 3 minutes
+                        adapter.SelectCommand.CommandTimeout = 180;
+                        table = new DataTable();
+                        adapter.Fill(table);
+                    }
+                }
+            }
+
+            return table;
+        }
+    }
+}
