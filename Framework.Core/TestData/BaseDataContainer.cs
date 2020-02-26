81dc082a2dcbca9602da054066fc0ed78035c39a
Karthik Anbazhagan <Karthik.Anbazhagan@cognizant.com>
2020-02-26 10:58:27 +0530

    Added base data container

---
+++ Framework.Core/TestData/BaseDataContainer.cs
@@ -0,0 +1,30 @@
+﻿namespace Framework.Core.TestData
+{
+    using Framework.Core.Utilities;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    public abstract class BaseDataContainer<T> : IDataContainer<T> where T : class, new()
+    {
+        protected IList<T> Data;
+
+        protected string FilePath;
+
+        public void AddData(T item)
+        {
+            Data.Add(item);
+            CsvDataHandler.InsertData(FilePath, item);
+        }
+
+        public void ImportData()
+        {
+            var dataTable = CsvDataHandler.ReadData(FilePath);
+            Data = DataConverter.CreateListFromTable<T>(dataTable);
+        }
+
+        public void UpdateData(T item, PropertyInfo primaryKey)
+        {
+            CsvDataHandler.UpdateData(FilePath, primaryKey, item);
+        }
+    }
+}
---
+++ Framework.Core/TestData/IDataContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace Framework.Core.TestData
+{
+    using System.Reflection;
+
+    public interface IDataContainer<T> where T: class, new()
+    {
+        void ImportData();
+
+        void AddData(T item);
+
+        void UpdateData(T item, PropertyInfo primaryKey);
+    }
+}
