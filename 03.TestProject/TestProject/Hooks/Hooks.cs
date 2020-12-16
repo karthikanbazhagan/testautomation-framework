84c4dbebcb759c7e132223ccd802c0d55ff2c2b4
karthikanbazhagan <karthiksanjay@live.com>
2020-12-16 09:49:57 +0530

    Framework: TestProject initial setup.

---
+++ 03.TestProject/TestProject/Hooks/Hooks.cs
@@ -0,0 +1,34 @@
+﻿namespace TestProject.Hooks
+{
+    using BoDi;
+    using Framework.Core.Factory;
+    using OpenQA.Selenium;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class Hooks
+    {
+        private readonly WebDriverFactory _webdriverFactory;
+        private readonly IObjectContainer _objectContainer;
+        private IWebDriver _webdriver;
+
+        public Hooks(IObjectContainer objectContainer, WebDriverFactory webDriverFactory)
+        {
+            _objectContainer = objectContainer;
+            _webdriverFactory = webDriverFactory;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _webdriver = _webdriverFactory.GetWebDriver;
+            _objectContainer.RegisterInstanceAs<IWebDriver>(_webdriver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _webdriver.Quit();
+        }
+    }
+}
