250b7ee2a1af48fd18573e6adfc866f2f250d0c1
karthikanbazhagan <karthiksanjay@live.com>
2020-12-16 13:12:49 +0530

    TestProject: Initial configs and setup - tested.

---
+++ 03.TestProject/TestProject/Features/WikipediaTitleValidation.feature
@@ -0,0 +1,16 @@
+﻿Feature: Wikipedia title validation
+
+Scenario: Validate Google's Title
+	Given I want to validate the title of "Google" wikipedia page
+	When I navigate to the "Google" page on wikipedia
+	Then the the title of the page should be "Google - Wikipedia"
+
+Scenario: Validate Microsoft's Title
+	Given I want to validate the title of "Microsoft" wikipedia page
+	When I navigate to the "Microsoft" page on wikipedia
+	Then the the title of the page should be "Microsoft - Wikipedia"
+
+Scenario: Validate Apple's Title
+	Given I want to validate the title of "Apple" wikipedia page
+	When I navigate to the "Apple" page on wikipedia
+	Then the the title of the page should be "Apple - Wikipedia"
---
+++ 03.TestProject/TestProject/StepDefinitions/WikipediaTitleValidationSteps.cs
@@ -0,0 +1,43 @@
+﻿namespace TestProject.StepDefinitions
+{
+    using OpenQA.Selenium;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public sealed class WikipediaTitleValidationSteps
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly IWebDriver _webdriver;
+
+        public WikipediaTitleValidationSteps(IWebDriver webdriver)
+        {
+            _webdriver = webdriver;
+        }
+
+        [Given(@"I want to validate the title of ""(.*)"" wikipedia page")]
+        public void GivenIWantToValidateTheTitleOfWikipediaPage(string name)
+        {
+            Console.WriteLine($"To validate {name} title page");
+        }
+
+        [When(@"I navigate to the ""(.*)"" page on wikipedia")]
+        public void WhenINavigateToThePageOnWikipedia(string name)
+        {
+            _webdriver.Url = $"https://en.wikipedia.org/wiki/{name}";
+            _webdriver.Navigate();
+        }
+
+        [Then(@"the the title of the page should be ""(.*)""")]
+        public void ThenTheTheTitleOfThePageShouldBe(string title)
+        {
+            Xunit.Assert.Equal(title, _webdriver.Title);
+        }
+
+
+    }
+}
